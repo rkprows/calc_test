5308a3be91e4ea83a13cba1b5a1bcd5caf99969e
Rachel Prows <rachel.prows@icloud.com>
2016-05-12 14:14:56 -0600

    complete

--- calc2.rb
+++ calc2.rb
@@ -1,10 +1,3 @@
-# get first number
-# get operator
-# validate correct operator
-# get second number
-# print output
-# prompt to continue or exit
-
 
 def print_header
 	puts "Welcome to Ruby Calculator"
