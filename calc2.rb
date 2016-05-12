3352c4de52219dfee2de4d4d24bf72defdced18c
Rachel Prows <rachel.prows@icloud.com>
2016-05-12 14:12:02 -0600

    add all functionality

--- calc2.rb
+++ calc2.rb
@@ -12,4 +12,29 @@ def print_header
 end
 =====
 def run
+	print_header
+	first_num = get_num('first')
+	op = operator
+	second_num = get_num(second)
 end
+
+def get_num(place)
+	puts "Enter #{place} number"
+	gets.to_f
+end
+
+def operator
+valid = false
+	until valid
+	puts "Enter operator (+, -, /, *)"
+	op = gets.strip
+	valid = op =~ /\+|-|\*|\//
+	end
+	op
+end
+
+while true
+	run
+	puts "tye q to exit"
+	exit if q
+
